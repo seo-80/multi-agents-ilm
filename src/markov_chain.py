8198f144e74f6adc754eb8ce1ae0b5b917fd3cc1
seo-80 <80seo717123@gmail.com>
2024-03-21 11:26:43 +0900

    improve code

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -60,6 +60,27 @@ def transition_matrix(
     ])
     transition_matrix=transition_matrix.reshape([arg["data_size"]+1 for arg in agents_arguments]*2)
     return transition_matrix
+def transition_matrix(
+    agents_arguments,
+    agents_count=None,
+    network=None,
+):
+    if agents_count is None:
+        if not type(agents_arguments) == list:
+            raise ValueError
+        agents_count=len(agents_arguments)
+    if type(agents_arguments) == dict:
+        agents_arguments=[agents_arguments for _ in range(agents_count)]
+
+    #init network
+    if type(network) == dict or network is None:
+        network = ilm.networks.network(agents_count=agents_count,args=network)
+    transition_matrix=np.empty([arg["data_size"]+1 for arg in agents_arguments]*2)
+    for prestate in (itertools.product(*[range(arg["data_size"]+1) for arg in agents_arguments])):
+        for poststate in itertools.product(*[range(arg["data_size"]+1) for arg in agents_arguments]):
+            transition_matrix[poststate+prestate]=transition_probability(prestate, poststate, network, agents_arguments)
+
+    return transition_matrix
 
     
     
@@ -67,33 +88,42 @@ def transition_matrix(
 if __name__ == "__main__":#テスト
 
     agents_arguments=[
-        {"alpha":0.,"data_size":2},
-        {"alpha":0.01,"data_size":2},
-        {"alpha":0.,"data_size":2},
+        {"alpha":0.,"data_size":1},
+        {"alpha":0.,"data_size":1},
+        {"alpha":0.,"data_size":1},
+        {"alpha":0.1,"data_size":1},
+        {"alpha":0.,"data_size":1},
+        {"alpha":0.,"data_size":1},
+        {"alpha":0.,"data_size":1},
     ]
+
+    
     agents_count=len(agents_arguments)
     network=ilm.networks.network(agents_count=agents_count,args={
-        "outer_flow_rate":0.01
+        "bidimentional_flow_rate":0.1
     })
     print(network)
+    pri
     m=transition_matrix(
         agents_arguments=agents_arguments,
         network=network
     )
     data_sizes=np.array([arg["data_size"] for arg in agents_arguments], dtype=int)
 
+    simulation_count=10000
     import matplotlib.pyplot as plt
-    simulation_count=1000
     states=np.zeros([agents_arguments[i]["data_size"]+1 for i in range(agents_count)])
-    init_state=np.zeros(agents_count);init_state[agents_count//2]=1
-    states[(0,1,0)]=1#todo　agent数、初期条件一般化する
+    init_state=np.zeros(agents_count,dtype=int);init_state[agents_count//2]=1
+    states[tuple(init_state)]=1#todo　agent数、初期条件一般化する
+
     rai=1
-    print(m.shape)
+
     print("simulating....")
     states_record=np.empty((simulation_count,)+states.shape)
     for i in tqdm.tqdm(range(simulation_count)):
         states_record[i]=states
         states = np.tensordot(m,states, axes=(range(agents_count,agents_count*2),range(agents_count)))
+
 
     what_plot="distances"
     if what_plot=="variants_frequency":
@@ -107,18 +137,20 @@ if __name__ == "__main__":#テスト
         distances_matrix=np.empty((agents_count,)*2+states.shape)
         for index in itertools.product(*[range(ds) for  ds in distances_matrix.shape]):
             distances_matrix[index]=abs(index[index[0]+2]-index[index[1]+2])
+
         distances_record=np.tensordot(states_record,distances_matrix,axes=(range(1,agents_count+1),range(2,agents_count+2)))
         # distances_record=np.zeros((simulation_count,)+(agents_count,)*2)
         # for t in range(simulation_count):
         #     for index in itertools.product(*[range(ds) for  ds in distances_matrix.shape]):
         #         distances_record[(t,)+(index[0],index[1])]+=states_record[(t,)+index[2:]]*abs(index[index[0]+2]-index[index[1]+2])
 
-            
-        legend=[]
-        for ai in itertools.combinations(range(agents_count),2):
-            legend.append(str(ai[0])+str(ai[1]))
-            ax.plot(distances_record[:,ai[0],ai[1]])
-        ax.legend(legend)
-        plt.show()
-            
 
+
+        legend=[]
+        # for ai in itertools.combinations(range(agents_count),2):
+        #     legend.append(str(ai[0])+str(ai[1]))
+        #     ax.plot(distances_record[:,ai[0],ai[1]])
+        ax.invert_xaxis()
+        ax.pcolor(distances_record.sum(axis=0))
+        ax.legend(legend)
+        # plt.show()
